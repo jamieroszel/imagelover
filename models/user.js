7251817a340af8217ba445d976309288fe9e5a0d
jamieroszel <jamie.s.roszel@gmail.com>
2021-04-04 22:39:36 -0400

    first commit. still trying to style the images pages to make it organized.

---
+++ models/user.js
@@ -0,0 +1,23 @@
+// Import Schema and model
+const {Schema, model} = require('../db/connection')
+
+// The Image Schema
+const Image = new Schema ({
+    text: String
+})
+
+// The User Schema
+const UserSchema = new Schema({
+    username: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
+    // The image property defined as an array of objects that matches the Image schema
+    images: [Image],
+},
+{timestamps: true}
+)
+
+// The User Model
+const User = model("User", UserSchema)
+
+// Export the User Model
+module.exports = User;
